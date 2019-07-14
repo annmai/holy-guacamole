5042ab448a453a51c94825be6a7eb06695aaf280
Ann Mai <pumpkyn@Anns-MacBook.local>
2019-07-13 22:13:00 -0700

    init commit

---
+++ src/BandsServlet.java
@@ -0,0 +1,87 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+// Declaring a WebServlet called BandsServlet, which maps to url "/api/bands"
+@WebServlet(name = "BandsServlet", urlPatterns = "/api/bands")
+public class BandsServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.xml
+    @Resource(name = "jdbc/musicdb")
+    private DataSource dataSource;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+
+            String query = "SELECT * from bands";
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                String origin = rs.getString("origin");
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", id);
+                jsonObject.addProperty("name", name);
+                jsonObject.addProperty("origin", origin);
+
+                jsonArray.add(jsonObject);
+            }
+            
+            // write JSON string to output
+            out.write(jsonArray.toString());
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+            rs.close();
+            statement.close();
+            dbcon.close();
+        } catch (Exception e) {
+        	
+			// write error message JSON object to output
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			out.write(jsonObject.toString());
+
+			// set response status to 500 (Internal Server Error)
+			response.setStatus(500);
+
+        }
+        out.close();
+
+    }
+}
