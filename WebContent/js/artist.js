72e150d54704c9db832fcd31fa865f5ad509f8fb
annmai <pumpkyn33@gmail.com>
2019-07-26 14:55:15 -0700

    added artist albums

---
+++ WebContent/js/artist.js
@@ -0,0 +1,74 @@
+/**************************************************************************
+ * Gets parameter by name passed in URL
+ **************************************************************************/
+
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+
+/**************************************************************************
+ * Handles the data returned by the API, read the jsonObject and populate 
+ * data into html elements
+ * @param resultData jsonObject
+ **************************************************************************/
+function handleArtistResult(resultData) {
+    console.log("handleArtistResult: populating albums table from resultData");
+
+    // Populate the albums table
+    // Find the empty table body by id "star_table_body"
+    let albumsTableBodyElement = jQuery("#albums_table_body");
+    albumsTableBodyElement.empty();
+
+    // Iterate through resultData, no more than 10 entries
+    for (let i = 0; i < resultData.length; i++) {
+
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML +=
+            "<td>" + 
+            "<img id='album-pic' src='img/band-logos/" + artistId + ".jpg'>" +
+            // Add a link to album.html with id passed with GET url parameter
+            '<a href="album.html?id=' + resultData[i]['id'] + '">'
+            + resultData[i]["title"] +     // display album name for the link text
+            '</a>' +
+            "</td>";
+        rowHTML += "<td>" + resultData[i]["year"] + "</td>";
+        rowHTML += "<td>" + resultData[i]["label"] + "</td>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        albumsTableBodyElement.append(rowHTML);
+    }
+}
+
+
+
+
+/******************************************************************************
+ * Once this .js is loaded, following scripts will be executed by the browser
+ ******************************************************************************/
+
+let artistId = getParameterByName('id');
+
+// Makes the HTTP GET request and registers on success callback function handleArtistResult
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/artist?id=" + artistId, // Setting request url, which is mapped by ArtistServlet.java
+    success: (resultData) => handleArtistResult(resultData) // Setting callback function to handle data returned successfully by the ArtistServlet
+});
---
+++ src/SingleArtistServlet.java
@@ -0,0 +1,121 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class SingleArtistServlet
+ */
+@WebServlet(name = "ArtistServlet", urlPatterns = "/api/artist")
+public class SingleArtistServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.xml
+    @Resource(name = "jdbc/musicdb")
+    private DataSource dataSource;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SingleArtistServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/json"); // Response mime type
+        
+        String id = request.getParameter("id");
+        String query = "SELECT name from bands where id = '" + id + "'";
+       
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        try {
+            // Get a connection from dataSource
+            Connection dbcon = dataSource.getConnection();
+
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+            
+        
+            if(rs.next()) {
+            	String artist_name = rs.getString("name");
+            	query = "Select * from albums where artist = '" + artist_name + "' order by year asc";
+            	rs = statement.executeQuery(query);
+            }
+            
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String album_id = rs.getString("id");
+                String album_title = rs.getString("title");
+               String artist = rs.getString("artist");
+                String label = rs.getString("label");
+                int year = rs.getInt("year");
+                int num_disks = rs.getInt("number_of_disks");
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", album_id);
+                jsonObject.addProperty("title", album_title);
+                jsonObject.addProperty("artist", artist);
+                jsonObject.addProperty("label", label);
+                jsonObject.addProperty("year", year);
+                jsonObject.addProperty("num_disks", num_disks);
+
+                jsonArray.add(jsonObject);
+            }
+            
+            // write JSON string to output
+            out.write(jsonArray.toString());
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+            rs.close();
+            statement.close();
+            dbcon.close();
+        }
+            
+        catch (Exception e) {
+        	
+			// write error message JSON object to output
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			out.write(jsonObject.toString());
+			
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+
+			// set response status to 500 (Internal Server Error)
+			response.setStatus(500);
+
+        }
+        
+        out.close();
+
+    }
+
+}
