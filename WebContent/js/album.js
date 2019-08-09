d88008e0f09346c3639686240790c459ad9e62e3
annmai <pumpkyn33@gmail.com>
2019-08-09 14:18:40 -0700

    added cart btn

--- WebContent/js/album.js
+++ WebContent/js/album.js
@@ -32,6 +32,7 @@ function handleAlbumInfoResult(resultData) {
     
     let html = "<img id='album-picture' src='img/album-pics/" + id + ".jpg'>";
     html += "<br><br><div id='album-info'><b>" + resultData[0]["album_title"] + "</b><br>" + resultData[0]["artist"] + "<br>" + resultData[0]["label"] + " " + resultData[0]["year"] + "</div>";
+    html += "<br><button>addToCart</button>"
     albumContentDivElement.append(html);
     
     console.log(resultData.length);
