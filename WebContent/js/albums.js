4841989cbdbd6de9826077b5e30567620876031e
annmai <pumpkyn33@gmail.com>
2019-08-31 20:12:31 -0700

    removed fav heart

--- WebContent/js/albums.js
+++ WebContent/js/albums.js
@@ -1,4 +1,32 @@
+/******************************************************************
+ * Handles result data returned by FavoriteServlet
+ ******************************************************************/
+function handleFavResult(resultData) {
+	sessionStorage.setItem("favoriteItems", resultData);
+}
 
+
+/****************************************************************************
+ * Toggles like/unlike Favorite Heart 
+ ****************************************************************************/
+
+function toggleFavorite(e) {
+  var heart = e.getAttribute("data-fav-type");
+  var id = e.getAttribute("data-id");
+  var title = e.getAttribute("data-title");
+  var artist = e.getAttribute("data-artist");
+
+  if (heart == "unliked") {
+    e.innerHTML = "favorite";
+    e.setAttribute("data-fav-type", "like");
+    doFavPost(id, title, artist, "like")
+    
+  } else {
+	  e.innerHTML = "favorite_border";
+	  e.setAttribute("data-fav-type", "unliked");
+	  doFavPost(id, title, artist, "unliked")
+  }
+}
 
 /**************************************************************************
  * Handles the data returned by the API, read the jsonObject and populate 
@@ -7,8 +35,6 @@
  **************************************************************************/
 function handleAlbumsResult(resultData) {
 
-    // Populate the bands table
-    // Find the empty table body by id "star_table_body"
     let albumsGridBodyElement = jQuery("#album-list");
     albumsGridBodyElement.empty();
 
@@ -21,12 +47,26 @@ function handleAlbumsResult(resultData) {
         gridItemHTML += '<a href="album.html?id=' + resultData[i]["id"] + '">';
         gridItemHTML += '<img class="album-pic" src="img/album-pics/' + resultData[i]["id"] + '.jpg"></a><br>';
         gridItemHTML += '<p class="album-caption">' + resultData[i]["title"] + '<br>' + resultData[i]["artist"] + '</p>';
-        gridItemHTML += '<p><i class="material-icons fav-heart" data-fav-type="unliked" onclick="toggleFavorite(this)">favorite_border</i></p>';
+        //gridItemHTML += '<p><i class="material-icons fav-heart" data-id="' + resultData[i]["id"] + '" data-title="' + resultData[i]["title"] + '" data-artist="' + resultData[i]["artist"] + '" data-fav-type="unliked" onclick="toggleFavorite(this)">favorite_border</i></p>';
         gridItemHTML += '</div>';
         
         // Append the grid item created to the grid container, which will refresh the page
         albumsGridBodyElement.append(gridItemHTML);
     }
+}
+
+/********************************************************************
+ * Updates favorites, liked/unliked albums, to the backend by making 
+ * a POST request
+ *********************************************************************/
+function doFavPost(albumId, albumTitle, albumArtist, op) {
+	  $.ajax({
+		    url: 'api/fav',
+		    type: 'POST',
+		    data: jQuery.param({ id: albumId, title: albumTitle, artist: albumArtist, operation: op}) ,
+		    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		    success: (resultData) => handleFavResult(resultData)
+		}); 
 }
 
 
